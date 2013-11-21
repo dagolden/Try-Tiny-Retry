c2e0f67603f8bc48c13123ba3ffffd873ab8b1eb
David Golden <dagolden@cpan.org>
2013-11-21 16:09:54 -0500

    document all functions

--- lib/Try/Tiny/Retry.pm
+++ lib/Try/Tiny/Retry.pm
@@ -27,25 +27,6 @@ sub delay(&;@) { ## no critic
     return ( bless( \$block, 'Try::Tiny::Retry::Delay' ), @rest, );
 }
 
-=func delay_exp
-
-    retry { ... }
-    delay_exp [ 3, 10000 ] # 3 tries, 10000 µsec
-    catch { ... };
-
-This function is an exponential-backoff delay-function generator.  The delay
-between attempts is randomly selected between 0 and an upper bound. The upper
-bound doubles after each failure.
-
-It requires an array reference as an argument. The first element is the number
-of tries allowed.  The second element is the starting upper bound in
-microseconds.
-
-Given number of tries C<N> and upper bound C<U>, the expected cumulative
-delay time if all attempts fail is C<0.5 * U * ( 2^(N-1) - 1 )>.
-
-=cut
-
 sub delay_exp(&;@) { ## no critic
     my ( $params, @rest ) = @_;
 ##    croak "delay_exp requires an array reference argument"
@@ -160,16 +141,16 @@ Or, you can retry only if the error matches some conditions:
 
     use Try::Tiny::Retry;
 
-    retry   { ... }
-    retry_if { /could not connect/ }
-    catch { ... };
+    retry     { ... }
+    retry_if  { /^could not connect/ }
+    catch     { ... };
 
 Or, you can customize the number of tries and delay timing:
 
     use Try::Tiny::Retry ':all';
 
     retry       { ... }
-    delay_exp   [ 5, 1e6 ] # 5 tries, 1 second exponential-backoff
+    delay_exp   { 5, 1e6 } # 5 tries, 1 second exponential-backoff
     catch       { ... };
 
 =head1 DESCRIPTION
@@ -188,6 +169,77 @@ you need:
 
     use Try::Tiny;
     use Try::Tiny::Retry qw/retry delay_exp/;
+
+=func retry
+
+    retry    { ... }  # code that might fail
+    retry_if { ... }  # conditions to be met for a retry
+    delay    { ... }  # control repeats and intervals between retries
+    catch    { ... }; # handler if all retries fail
+
+The C<retry> function works just like C<try> from L<Try::Tiny>, except that if
+an exception is thrown, the block may be executed again, depending on the
+C<retry_if> and C<delay> blocks.
+
+By default, if no C<retry_if> blocks are provided, retries depend only on
+the C<delay> block.
+
+If no C<delay> block is provided, the default will be 10 tries with a random
+delay up to 100 milliseconds with an exponential backoff.  (See L</delay_exp>.)
+This has an expected cumulative delay of around 25 seconds if all retries fail.
+
+=func retry_if
+
+    retry    { ... }
+    retry_if { /^could not connect/ }
+    catch    { ... };
+
+A C<retry_if> block controls whether a retry should be attempted.
+
+The block is passed the cumulative number of attempts as an argument.  The
+exception caught is provided in C<$_>, just as with C<catch>.  It should
+return a true value if a retry should be attempted.
+
+Multiple C<retry_if> blocks may be provided.  Only one needs to evaluate
+to true to enable a retry.
+
+=func delay
+
+    retry { ... }
+    delay {
+        return if $_[0] >= 3; # only three tries
+        sleep 1;              # constant delay between tries
+    }
+    catch { ... };
+
+The C<delay> block is executed when the C<retry> block throws an exception
+to determine if a retry should be attempted and to manage the delay between
+attempts.
+
+The block is passed the cumulative number of attempts as an argument.  If the
+C<delay> block returns an undefined value, no further retries will be made and
+the most recent exception will be rethrown.
+
+Only one C<delay> block is allowed.
+
+=func delay_exp
+
+    retry     { ... }
+    delay_exp { 3, 10000 } # 3 tries, 10000 µsec
+    catch     { ... };
+
+This function is an exponential-backoff delay-function generator.  The delay
+between attempts is randomly selected between 0 and an upper bound. The upper
+bound doubles after each failure.
+
+It requires a code block as an argument.  The block will be evaluated in list
+context and must return two elements.  The first element is the number of tries
+allowed.  The second element is the starting upper bound in B<microseconds>.
+
+Given number of tries C<N> and upper bound C<U>, the expected cumulative
+delay time if all attempts fail is C<0.5 * U * ( 2^(N-1) - 1 )>.
+
+=cut
 
 =head1 SEE ALSO
 
